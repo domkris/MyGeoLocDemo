1dc19726b52c5f8eda97d755239d17cade860a51
domagojkristov <domagojkk@gmail.com>
2020-12-03 18:46:54 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {} from 'googlemaps';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    this.initMap()
+  }
   title = 'MyGeoLocDemo';
+  map;
+  initMap(): void {
+    this.map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: 42.397, lng: -70 },
+      zoom: 8,
+    });
+  }
 }
