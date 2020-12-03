eb4624adfc773ca1883b766d0522b60fed5f92f3
domagojkristov <domagojkk@gmail.com>
2020-12-03 22:21:55 +0100

    5th commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import {} from 'googlemaps';
 
@@ -7,6 +8,11 @@ import {} from 'googlemaps';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
+  places: any;
+  url;
+  latitude: number;
+  longitude: number;
+  constructor(private http: HttpClient){}
   ngOnInit(): void {
     this.initMap()
   }
@@ -27,11 +33,64 @@ export class AppComponent implements OnInit{
     this.infoWindow.open(this.map);
   }
   findPlaces(){
+    const url = 'https://localhost:44311/places/search?location=43.5081,16.4402&radius=1500&type=restaurant&keyword=cruise';
+    //const url = 'https://localhost:44311/places/search?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise';
+    // this.url = 'https://localhost:44311/places/search?location=' + this.latitude + ',' + this.longitude + 
+    // '&radius=1500&type=restaurant&keyword=cruise';
+    const promise = this.http.get(url).toPromise();
+    promise.then((data)=>{
+      //console.log(data);
+      this.places = data;
+      console.log(this.places);
 
+      this.places.forEach(place => {
+
+        //console.log(typeof place.geometry.location.latitude);
+        //console.log(typeof place.geometry.location.longitude);
+
+        const pos = {
+          lat: place.geometry.location.latitude,
+          lng: place.geometry.location.longitude
+        };
+        //this.addMarker(pos);
+        this.addMarkers(place);
+      });
+    })
   }
   onKey($event){
     this.searchPlace = $event.target.value;
     console.log(this.searchPlace);
+  }
+  addMarkers(place){
+    var icon = {
+      url: place.icon, // url
+      scaledSize: new google.maps.Size(25, 25), // scaled size
+      origin: new google.maps.Point(0,0), // origin
+      anchor: new google.maps.Point(0, 0) // anchor
+    };
+    const pos = {
+      lat: place.geometry.location.latitude,
+      lng: place.geometry.location.longitude
+    };
+    var marker = new google.maps.Marker({
+      position: pos,
+      map: this.map,
+      draggable: true,
+      icon: icon,
+      animation: google.maps.Animation.DROP
+    })
+    var infoWindow = new google.maps.InfoWindow({
+      content: '<h1>' +  place.name + '</h1> <h2>' +  place.vicinity + '</h2> '
+    });
+    var clicked = false;
+    marker.addListener('click', ()=>{
+      if(clicked){
+        infoWindow.close();
+      }else{
+        infoWindow.open(this.map, marker);
+      }
+      clicked = !clicked;
+    });
   }
   addMarker(coords){
     var marker = new google.maps.Marker({
@@ -55,6 +114,8 @@ export class AppComponent implements OnInit{
             zoom: 12,
           });
           this.addMarker(pos);
+          this.latitude = position.coords.latitude;
+          this.longitude = position.coords.longitude;
         },
         () => {
           this.handleLocationError(true, this.map.getCenter());
